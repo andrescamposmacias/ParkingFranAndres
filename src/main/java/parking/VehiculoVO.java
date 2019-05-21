46042152022105fae4dce99b3514d92ba5e72224
FranAragon13 <franestepona199813@hotmail.com>
2019-05-21 19:48:27 +0200

    Crea clase Vehiculo y añade atributos y métodos

---
+++ src/main/java/parking/VehiculoVO.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parking;
+
+/**
+ *
+ * @author fran
+ */
+public class VehiculoVO {
+
+    //Atributos
+    private String matricula;
+    private String tipo;
+
+    //Métodos
+    //Constructor parametrizado
+    public VehiculoVO(String matricula, String tipo) {
+        this.matricula = matricula;
+        this.tipo = tipo;
+    }
+
+    //Constructor por defecto
+    public VehiculoVO() {
+    }
+
+    //Getter&Setter
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    //toString
+    @Override //Sobrescrito
+    public String toString() {
+        return "Vehiculo{" + "matricula=" + matricula + ", tipo=" + tipo + '}';
+    }
+
+}
