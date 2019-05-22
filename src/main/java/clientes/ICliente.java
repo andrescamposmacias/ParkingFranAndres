2f9866c2688413fea8684af39ea8f9158d6b6744
franaragon13 <franaragon196913@gmail.com>
2019-05-22 13:20:08 +0200

    Crea interfaz con métodos para cliente

---
+++ src/main/java/clientes/ICliente.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientes;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author fran
+ */
+public interface ICliente {
+    
+    // Méodo para obtener un registro a partir de la PK
+    ClientesVO buscarDni(String dni) throws SQLException;
+    
+    // Método para insertar un registro
+    int insertCliente (ClientesVO cliente) throws SQLException;
+    
+    // Método para insertar varios registros
+    int insertCliente (List<ClientesVO> lista) throws SQLException;
+    
+    // Método para borrar una persona
+    int deleteCliente (ClientesVO cliente) throws SQLException;
+    
+    // Método para modificar una persona. Se modifica a la persona que tenga esa 'pk'
+    // con los nuevos datos que traiga la persona 'nuevosDatos'
+    int updateCliente (int dni, ClientesVO cliente) throws SQLException;
+    
+}
