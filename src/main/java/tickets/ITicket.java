943787a858df8094d296d34ae9981e6ff33c7527
Andres <andrescamposmacias@gmail.com>
2019-05-25 19:36:11 +0200

    añadir 2 nuevos metodos y cambiar el tipo de devolucion de los metodos

--- src/main/java/tickets/ITicket.java
+++ src/main/java/tickets/ITicket.java
@@ -18,7 +18,13 @@ public interface ITicket {
     List<TicketsVO> getAll() throws SQLException;
     
         // Méodo para obtener un registro a partir de la PK
-    TicketsVO buscarTickets(String matricula) throws SQLException;
+    boolean buscarTicketsMatricula(String matricula) throws SQLException;
+    
+       // Méodo para obtener un registro a partir de la PK
+    boolean buscarTicketsPin(int pin) throws SQLException;
+    
+       // Méodo para obtener un registro a partir de la PK
+    boolean buscarTicketsNumeroPlaza(String numeroPlaza) throws SQLException;
     
     // Método para insertar un registro
     int insertTickets(TicketsVO ticket) throws SQLException;
