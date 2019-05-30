b3a7f5ab45888e6a3c7eab2d4ce55e9e350a1ae0
Andres <andrescamposmacias@gmail.com>
2019-05-31 01:35:54 +0200

    añadir metodo insertVehiculoPersonaSinAbono()

--- src/main/java/vehiculos/VehiculoDAO.java
+++ src/main/java/vehiculos/VehiculoDAO.java
@@ -13,7 +13,14 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 import conexion.Conexion;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Scanner;
+import plazas.PlazaDAO;
+import tickets.TicketsDAO;
+import tickets.TicketsVO;
 
 /**
  *
@@ -26,7 +33,7 @@ public class VehiculoDAO implements IVehiculo {
     public VehiculoDAO() {
         con = Conexion.getInstance();
     }
-    
+
     @Override
     public List<VehiculoVO> getAll() throws SQLException {
         List<VehiculoVO> lista = new ArrayList<>();
@@ -50,7 +57,7 @@ public class VehiculoDAO implements IVehiculo {
 
         return lista;
     }
-    
+
     @Override
     public VehiculoVO findByMatricula(String matricula) throws SQLException {
 
@@ -175,7 +182,7 @@ public class VehiculoDAO implements IVehiculo {
             return numFilas;
         }
     }
-    
+
     @Override
     public int deleteVehiculos() throws SQLException {
         String sql = "delete from vehiculo";
@@ -192,4 +199,98 @@ public class VehiculoDAO implements IVehiculo {
         // El borrado se realizó con éxito, devolvemos filas afectadas
         return nfilas;
     }
+
+    public boolean insertVehiculoPersonaSinAbono() throws SQLException {
+
+        Scanner teclado = new Scanner(System.in);
+
+        TicketsDAO tickets = new TicketsDAO();
+        PlazaDAO plaza = new PlazaDAO();
+
+        //atributos vehiculo
+        String matriculaRegistro;
+        String tipoVehiculo;
+
+        String numeroPlazaRegistro="";
+
+        System.out.println("Introduzca su matricula");
+        matriculaRegistro = teclado.nextLine();
+
+        do {
+            System.out.println("Introduce el tipo de vehículo (Turismo, Motocicleta o Caravana)");
+            tipoVehiculo = teclado.nextLine();
+        } while (!tipoVehiculo.equalsIgnoreCase("Turismo") && !tipoVehiculo.equalsIgnoreCase("Motocicleta") && !tipoVehiculo.equalsIgnoreCase("Caravana"));
+
+        if (tipoVehiculo.equalsIgnoreCase("Turismo")) {
+            if (plaza.plazaTurismoLibre().equalsIgnoreCase("0")) {
+                System.out.println("Lo siento no quedan plazas de turismo");
+                return false;
+            } else {
+                numeroPlazaRegistro = plaza.sacarNumeroPlazaTurismo();
+                plaza.updatePlazaAbonadoRetirado(numeroPlazaRegistro);
+            }
+        }
+
+        if (tipoVehiculo.equalsIgnoreCase("Motocicleta")) {
+            if (plaza.plazaMotocicletaLibre().equalsIgnoreCase("0")) {
+                System.out.println("Lo siento no quedan plazas de Motocicleta");
+                return false;
+            } else {
+                numeroPlazaRegistro = plaza.sacarNumeroPlazaMotocicleta();
+                plaza.updatePlazaAbonadoRetirado(numeroPlazaRegistro);
+            }
+        }
+
+        if (tipoVehiculo.equalsIgnoreCase("Caravana")) {
+            if (plaza.plazaCaravanaLibre().equalsIgnoreCase("0")) {
+                System.out.println("Lo siento no quedan plazas de Caravana");
+                return false;
+            } else {
+                numeroPlazaRegistro = plaza.sacarNumeroPlazaCaravana();
+                plaza.updatePlazaAbonadoRetirado(numeroPlazaRegistro);
+            }
+        }
+
+        String sql = "insert into vehiculo values (?,?)";
+
+        if (findByMatricula(matriculaRegistro) != null) {
+            // Existe un registro con esa pk
+            // No se hace la inserción
+            return false;
+        } else {
+            // Instanciamos el objeto PreparedStatement para inserción
+            // de datos. Sentencia parametrizada
+            try (PreparedStatement prest = con.prepareStatement(sql)) {
+
+                // Establecemos los parámetros de la sentencia
+                prest.setString(1, matriculaRegistro);
+                prest.setString(2, tipoVehiculo);
+
+                prest.executeUpdate();
+            }
+
+        }
+
+        //atributos del no abonado
+        LocalDate hoyRegistro = LocalDate.now();
+        LocalTime hoyHora = LocalTime.now();
+        LocalDate finalRegistro = LocalDate.now();
+        LocalTime finalHora = LocalTime.now();
+        int pinNoAbonado = tickets.generacionPinNoAbonado();
+        
+        LocalDateTime fechaYHora = LocalDateTime.of(hoyRegistro, hoyHora);
+        
+        System.out.println("El ticket generado es:");
+        System.out.println("La matricula del coche es: " + matriculaRegistro);
+        System.out.println("La fecha y hora de ingreso es: " + fechaYHora);
+        System.out.println("El numero de su plaza es: " + numeroPlazaRegistro);
+        System.out.println("Su pin para retirar el coche es: " + pinNoAbonado);
+        
+        tickets.insertTickets(new TicketsVO(hoyRegistro,finalRegistro,hoyHora,finalHora,0.0,pinNoAbonado,matriculaRegistro,numeroPlazaRegistro));
+        
+        plaza.updatePlazaNoAbonadoIngreso(matriculaRegistro);
+        
+        return true;
+    }
+
 }
